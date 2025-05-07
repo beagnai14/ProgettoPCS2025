0973a68650d77315979082c92d7598e51121a2f6
Beatrice Gnai <s310738@studenti.polito.it>
2025-05-07 10:49:17 +0200

    inizial commit

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "PolygonalMesh.hpp"
+#include "Utils.hpp"
+#include "UCDUtilities.hpp"
+
+using namespace std;
+using namespace Eigen;
+using namespace PolygonalLibrary;
+
+
+int main()
+{   
+
+    return 0;
+}
